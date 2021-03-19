cc931f59c5690000c0efec644c32e83806dd9751
nikhil <ch17btech11023@iith.ac.in>
2021-03-19 06:12:48 +0000

    Update cli

--- cli/cli.py
+++ cli/cli.py
@@ -3,7 +3,6 @@
 import itertools
 import os
 import sys
-import threading
 import time
 
 import click
@@ -36,75 +35,6 @@ class MutuallyExclusiveOption(click.Option):
         return super(MutuallyExclusiveOption, self).handle_parse_result(ctx, opts, args)
 
 
-# https://github.com/click-contrib/click-spinner
-class Spinner(object):
-    spinner_cycle = itertools.cycle(["-", "/", "|", "\\"])
-
-    def __init__(self, beep=False, disable=False, force=False, stream=sys.stdout):
-        self.disable = disable
-        self.beep = beep
-        self.force = force
-        self.stream = stream
-        self.stop_running = None
-        self.spin_thread = None
-
-    def start(self):
-        if self.disable:
-            return
-        if self.stream.isatty() or self.force:
-            self.stop_running = threading.Event()
-            self.spin_thread = threading.Thread(target=self.init_spin)
-            self.spin_thread.start()
-
-    def stop(self):
-        if self.spin_thread:
-            self.stop_running.set()
-            self.spin_thread.join()
-
-    def init_spin(self):
-        while not self.stop_running.is_set():
-            self.stream.write(next(self.spinner_cycle))
-            self.stream.flush()
-            self.stop_running.wait(0.25)
-            self.stream.write("\b")
-            self.stream.flush()
-
-    def __enter__(self):
-        self.start()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.disable:
-            return False
-        self.stop()
-        if self.beep:
-            self.stream.write("\7")
-            self.stream.flush()
-        return False
-
-
-def spinner(beep=False, disable=False, force=False, stream=sys.stdout):
-    """This function creates a context manager that is used to display a
-    spinner on stdout as long as the context has not exited.
-    The spinner is created only if stdout is not redirected, or if the spinner
-    is forced using the `force` parameter.
-    Parameters
-    ----------
-    beep : bool
-        Beep when spinner finishes.
-    disable : bool
-        Hide spinner.
-    force : bool
-        Force creation of spinner even when stdout is redirected.
-    Example
-    -------
-        with spinner():
-            do_something()
-            do_something_else()
-    """
-    return Spinner(beep, disable, force, stream)
-
-
 @click.group(context_settings=CONTEXT_SETTINGS, cls=DYMGroup)
 @click.option("--debug/--no-debug", default=False, show_default=True)
 @click.option(
@@ -132,7 +62,8 @@ def help(ctx):
 
 
 @main.command()
-def add():
+@click.pass_context
+def add(ctx):
     click.echo("Infer")
 
 
@@ -152,20 +83,27 @@ def augment(ctx, local, src):
 
 
 @main.command()
-def train():
+@click.pass_context
+def train(ctx):
     click.echo("Infer")
 
 
 @main.command()
-def infer():
+@click.pass_context
+def infer(ctx):
     click.echo("Infer")
 
 
-@main.command()
-def show():
-    with spinner(disable=True):
-        time.sleep(2.4)
-        click.echo("Infer")
+@main.group()
+@click.pass_context
+def show(ctx):
+    click.echo("Infer")
+
+
+@show.command()
+@click.pass_context
+def dataset(ctx):
+    click.echo("Infer")
 
 
 if __name__ == "__main__":
