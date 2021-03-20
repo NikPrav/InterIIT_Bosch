ab4d5cba8473ad1d2b55d50ef4e339239c135e43
NikPrav <nikhil.pravin@gmail.com>
2021-03-20 14:07:21 +0530

    made rudimentary landing page

--- web/src/Components/Landing/Landing.js
+++ web/src/Components/Landing/Landing.js
@@ -1,11 +1,47 @@
 import react from "react";
-import {Button} from 'antd';
+import {Button, PageHeader, Typography} from 'antd';
+import Layout, { Content, Footer, Header } from "antd/lib/layout/layout";
+import { FontColorsOutlined } from "@ant-design/icons";
+import "./styles.css";
+// import Title from "antd/lib/skeleton/Title";
+const { Title } = Typography
+// const { Header, Content, Footer } = Layout;
+
 
 function Landing() {
     return (
-        <Button href="/editor">
-            Login
-        </Button>
+        <Layout className='fullpage'>
+            {/* <PageHeader className = 'site-page-header' title = 'InterIIT Bosch Traffic Sign Detector' textAlign='center'>
+
+            </PageHeader> */}
+            <Header classname = 'site-leayout-subheader-background'>
+                <Title level={2}  style = {{color:'white', padding:'10px' }} >
+                    InterIIT Bosch Traffic Sign Recogniser
+                </Title>
+            </Header>
+
+
+            <Content className="site-layout-content" >
+                <div class='center'>
+                    <Button type='primary' href="/editor" size = 'large'>
+                        Login
+                    </Button>
+                    <div style={{textAlign:'center'}}>
+                        Press button to login, and add more text to make it look cool
+                    </div>
+                </div>
+            </Content>
+
+            <Footer className='site-layout-footer' >
+                <div>
+                <a style={{float:'left'}} href='https://github.com/npalladium/InterIIT-Bosch' >Github link</a> 
+                <b style={{float:'right'}}>IIT Hyderabad,2021</b>
+                </div>
+                
+            </Footer>
+
+        </Layout>
+        
     );
 }
 
