8870305606346d54fe1f79e0b03700f0e519fcb2
NikPrav <nikhil.pravin@gmail.com>
2021-03-22 00:12:24 +0530

    [WIP] added radio buttons to image card display

--- web/src/Components/PopupImage/PopupImage.js
+++ web/src/Components/PopupImage/PopupImage.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
-import {Card, Modal, Button, Image} from "antd";
+import {Card, Modal, Button, Image, Radio} from "antd";
 import img from "./stop.png";
 
 function PopupImage(props) {
   const source = props.source;
   const {Meta} = Card;
   const [isModalVisible, setisModalVisible] = useState(false);
+  const [ImageType, setImageType] = useState('dc')
   const showModal = () => {
     setisModalVisible(true);
   };
+
+  const handleTypeofImage = e => {
+    // setImageType(ImageType)
+    console.log('radio checked', e.target.value);
+    setImageType(e.target.value);
+  }
 
   const handleOk = () => {
     setisModalVisible(false);
@@ -32,9 +39,14 @@ function PopupImage(props) {
         onCancel={handleCancel}
       >
         <div style={{display: "flex", flexDir: "row"}}>
-          <Image style={{maxHeight: 600}} src={img} />
-          <Card title="Metadata" height={600}>
-            <p>Metadata</p>
+          <Image style={{maxHeight: 600 }} src={img} />
+          <Card title="Metadata" style = {{height:300,width:500}}>
+            <p>Training Preference</p>
+            <Radio.Group onChange={handleTypeofImage} value={ImageType}>
+              <Radio value={'train'}>Train</Radio>
+              <Radio value={'test'}>Test</Radio>
+              <Radio value={'dc'}>Don't Care</Radio>
+            </Radio.Group>
           </Card>
         </div>
       </Modal>
