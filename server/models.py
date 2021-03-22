c8cbcab204fa2f5be0183fe5ede07a4035d202b3
nikhil <ch17btech11023@iith.ac.in>
2021-03-22 11:56:38 +0000

    Add models

--- server/models.py
+++ server/models.py
@@ -1,11 +1,48 @@
 import mongoengine as me
 from mongoengine import Document, EmbeddedDocument, connect
 from mongoengine.errors import NotUniqueError, OperationError
-from mongoengine.fields import (BaseField, DateTimeField, DictField, ListField,
-                                StringField)
+from mongoengine.fields import (
+    BaseField,
+    DateTimeField,
+    DictField,
+    ListField,
+    EmailField,
+    ObjectIdField,
+    StringField,
+)
+
+from configs import cnf
+
+connect(db=cnf.APP_CONFIG.DB)
+
+
+class Info(Document):
+    _id = ObjectIdField()
+    uneditable_workspaces = ListField(StringField())
+
+
+class Workspace(Document):
+    _id = ObjectIdField()
+    name = StringField(required=True)
+    datasets = ListField(StringField())
+    added_images = DictField()
+    augmentations = DictField()
+    model_settings = DictField()
+    model_results = DictField()
 
 
 class Dataset(Document):
+    _id = ObjectIdField()
     name = StringField(required=True)
-    original_images = ListField(StringField())
-    augmented_images = DictField()
+
+
+class Globals(Document):
+    _id = ObjectIdField()
+    name = StringField(required=True)
+    value = StringField(required=True)
+
+
+class User(Document):
+    _id = ObjectIdField()
+    email = EmailField()
+    token = StringField()
