53f12ed59b3cbb3e27bb15bf448de65cc4209126
NikPrav <nikhil.pravin@gmail.com>
2021-03-24 21:03:17 +0530

    [WIP] Added auth code to cli
    - Need to fix the backend to accept both emails/user.subs

--- cli/cli.py
+++ cli/cli.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from auth0.v3.authentication import Social,GetToken, Database
+
 import itertools
 import os
 import sys
 import threading
 import time
+import urllib
 
 import click
 import requests
@@ -15,7 +18,14 @@ from click_didyoumean import DYMGroup
 # config = configparser.ConfigParser()
 # config.read('~/.config/.cbtsrc')
 
+########################### CHANGE THESE!1!1!1!1! #############################
+AUTHO_ACCOUNT = 'dev-kqx4v2yr.jp.auth0.com'
+CLIENT_ID="DBJyWZCoiZFCccUM5C50YYSPrBXn08oL"
+CLIENT_SECRET='HnKdfJzvT3YHoiu0hI_rYyWBtUiwujzHBngNp0jYf2eN1WyIfnYK7avlLBl-wYPu'
+UTH0_DOMAIN = 'dev-kqx4v2yr.jp.auth0.com'
+########################### CHANGE THESE #############################
 ACCESS_KEY = os.getenv("ACCESS_KEY")
+USERNAME = os.getenv('USERNAME_CLI')
 SECRET_ACCESS_KEY = os.getenv("SECRET_ACCESS_KEY")
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help", "help"])
@@ -105,6 +115,9 @@ def spinner(beep=False, disable=False, force=False, stream=sys.stdout):
     return Spinner(beep, disable, force, stream)
 
 
+
+
+
 @click.group(context_settings=CONTEXT_SETTINGS, cls=DYMGroup)
 @click.option("--debug/--no-debug", default=False, show_default=True)
 @click.option(
@@ -121,8 +134,29 @@ def spinner(beep=False, disable=False, force=False, stream=sys.stdout):
     cls=MutuallyExclusiveOption,
     mutually_exclusive=["gui"],
 )
-def main(debug, gui, json):
+
+@click.option(
+    '--reg_stat',
+    default = False,
+    show_default=True,
+    help = 'Set to true to register new user. Make sure that your password has atleast 8 character, a lowercase letter, a special character and a number'
+)
+
+@click.password_option()
+# Auth0 Commands
+
+
+
+def main(debug, gui, json,reg_stat,password):
+    click.echo(reg_stat)
+    token = ''
+    if reg_stat:
+        token = reg_new_user(password)
+    else:
+        token = authHandler(password)
     return
+
+    
 
 
 @main.command(hidden=True)
@@ -135,6 +169,59 @@ def help(ctx):
 def add():
     click.echo("Infer")
 
+# @main.command()
+def authHandler(pwd):
+    social = Social(AUTHO_ACCOUNT)
+    token = GetToken(AUTHO_ACCOUNT)
+    url = 'https://dev-kqx4v2yr.jp.auth0.com/oauth/token'
+    header = {'content-type': 'application/x-www-form-urlencoded'}
+    # scope = "appstore::apps:r eadwrite"
+    grant_type = "client_credentials"
+    click.echo(USERNAME)
+    data = {
+        "grant_type": grant_type,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+        # "scope": scope
+    }
+    auth = {}
+    try:
+        auth_token = requests.post(url,data=data,headers = header)
+        # auth_token = auth_token.json()
+        auth = token.login(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, username=USERNAME, password=pwd, realm='Username-Password-Authentication',scope ="read:current_user",audience='https://dev-kqx4v2yr.jp.auth0.com/api/v2/')
+        click.echo('Successfully verified!!')
+        click.echo(auth)
+#         curl --request GET \
+#       --url 'https://dev-kqx4v2yr.jp.auth0.com/api/v2/users?q=email%3A%22jane%40exampleco.com%22&search_engine=v3' \
+#       --header 'authorization: Bearer YOUR_MGMT_API_ACCESS_TOKEN'
+        # tk = auth.get('access_token')
+        # uname = urllib.parse.quote(USERNAME)
+        # click.echo(f'ename:{uname}')
+        # auth_id = requests.get(f'https://dev-kqx4v2yr.jp.auth0.com/api/v2/users?q=email%3A%22{uname}%22&search_engine=v3',
+        #     headers = {'Authorization' : f'Bearer {tk}'})
+        # click.echo(auth_id)
+        # auth_id = auth_id.json()
+        # auth_id = auth_id.get('identities').get('user_id')
+        # click.echo(auth_id)
+    except Exception as e:
+        click.echo(e)
+
+    return auth.get('access_token')
+
+def reg_new_user(pwd):
+    database = Database(AUTHO_ACCOUNT)
+    token = GetToken(AUTHO_ACCOUNT)
+    try:
+        status = database.signup(client_id=CLIENT_ID, email=USERNAME, password=pwd, connection='Username-Password-Authentication')
+        auth = token.login(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, username=USERNAME, password=pwd, realm='Username-Password-Authentication',scope ="read:current_user",audience='https://dev-kqx4v2yr.jp.auth0.com/api/v2/')
+        click.echo(auth)
+        click.echo(f'Success:{status}')
+    except Exception as e:
+        click.echo(f'Failure:{e}')
+
+    return auth.get('access_token')
+
+
 
 @main.command()
 @click.pass_context
@@ -146,9 +233,10 @@ def add():
     help="Create modified images locally",
     show_default=True,
 )
-@click.argument("src", nargs=-1)
-def augment(ctx, local, src):
-    click.echo(ctx)
+# @click.argument("src", nargs=-1)
+# def augment(ctx, local, src):
+#     click.echo(ctx)
+
 
 
 @main.command()
@@ -167,6 +255,15 @@ def show():
         time.sleep(2.4)
         click.echo("Infer")
 
+@main.command()
+def sendPrivateReq(token):
+    UrlToSendDataTo = 'http://localhost:5000/api/private'
+    data = requests.get(url=UrlToSendDataTo,
+     headers = {'Authorization' : f'Bearer {token}','UserEmail' : USERNAME})
+    click.echo(data)
+
+
+    
 
 if __name__ == "__main__":
     main()
