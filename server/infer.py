f74c4324da21151bda3af685e0c203630623c777
sandeep5500 <cs18btech11041@iith.ac.in>
2021-03-26 00:20:36 +0530

    Infer.py changes

--- server/infer.py
+++ server/infer.py
@@ -9,10 +9,11 @@ import pandas as pd
 from __future__ import print_function
 import zipfile
 import os
-import Net #LOAD LATEST NETWORK
+# import Net #LOAD LATEST NETWORK
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from utils import path_to_base64 #CHECK IF UTILS PATH IS CORRECT
+from config import cnf
 
 dim = 53 #LINK NUMBER OF CLASSES
 
@@ -28,9 +29,92 @@ data_transforms = transforms.Compose([
     transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
 ])
 
+class Net(nn.Module):
+    	def __init__(self,out_dim):
+		super(Net,self).__init__()
+		# CNN layers
+		self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
+		self.bn1 = nn.BatchNorm2d(100)
+		self.conv2 = nn.Conv2d(100, 150, kernel_size=3)
+		self.bn2 = nn.BatchNorm2d(150)
+		self.conv3 = nn.Conv2d(150, 250, kernel_size=3)
+		self.bn3 = nn.BatchNorm2d(250)
+		self.conv_drop = nn.Dropout2d()
+		self.fc1 = nn.Linear(250*2*2, 350)
+		self.fc2 = nn.Linear(350, out_dim)
+
+		self.localization = nn.Sequential(
+			nn.Conv2d(3, 8, kernel_size=7),
+			nn.MaxPool2d(2, stride=2),
+			nn.ReLU(True),
+			nn.Conv2d(8, 10, kernel_size=5),
+			nn.MaxPool2d(2, stride=2),
+			nn.ReLU(True)
+			)
+
+		# Regressor for the 3 * 2 affine matrix
+		self.fc_loc = nn.Sequential(
+			nn.Linear(10 * 4 * 4, 32),
+			nn.ReLU(True),
+			nn.Linear(32, 3 * 2)
+			)
+
+		# Initialize the weights/bias with identity transformation
+		self.fc_loc[2].weight.data.zero_()
+		self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+
+	# Spatial transformer network forward function
+	def stn(self, x):
+		xs = self.localization(x)
+		xs = xs.view(-1, 10 * 4 * 4)
+		theta = self.fc_loc(xs)
+		theta = theta.view(-1, 2, 3)
+		# print(theta.shape, x.shape)
+		grid = F.affine_grid(theta, x.size())
+		x = F.grid_sample(x, grid)
+		return x
+
+	def forward(self, x):
+		# transform the input
+		x = self.stn(x)
+
+		# Perform forward pass
+		x = self.bn1(F.max_pool2d(F.leaky_relu(self.conv1(x)),2))
+		x = self.conv_drop(x)
+		x = self.bn2(F.max_pool2d(F.leaky_relu(self.conv2(x)),2))
+		x = self.conv_drop(x)
+		x = self.bn3(F.max_pool2d(F.leaky_relu(self.conv3(x)),2))
+		x = self.conv_drop(x)
+		x = x.view(-1, 250*2*2)
+		x = F.relu(self.fc1(x))
+		x = F.dropout(x, training=self.training)
+		x = self.fc2(x)
+		return F.log_softmax(x, dim=1)
+
 
 model = Net(dim).to(device)
-model.load_state_dict(torch.load('model_0_0.9746770025839794', map_location = 'cpu')) #LOAD LATEST MODEL
+
+
+
+import os
+import time
+
+folder = cnf.MODELS_FOLDER
+
+files = [(f, os.path.getmtime(f)) for f in os.listdir(folder) 
+                if os.path.isfile(f)]
+
+files = sorted(files, key=lambda x: x[1], reverse=True)
+last_modified_file = None if len(files) == 0 else files[0][0]
+
+model_path = os.path.join(cnf.MODELS_FOLDER, last_modified_file)
+
+
+
+
+
+model.load_state_dict(torch.load(model_path, map_location = 'cpu')) #LOAD LATEST MODEL
 
 
 #CLASSWISE INACCURACIES
